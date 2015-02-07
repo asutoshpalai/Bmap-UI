3b6ec6efd6ef52309d4746439ae3041dc72c6c6e
Asutosh Palai <asupalai@gmail.com>
2015-02-07 19:45:57 +0530

    Map is sut up

--- BMap-UI/BMap-UI/pages/hub/TrackerPage.js
+++ BMap-UI/BMap-UI/pages/hub/TrackerPage.js
@@ -1,12 +1,30 @@
 ﻿(function () {
     "use strict";
 
-    var ControlConstructor = WinJS.UI.Pages.define("/pages/hub/section2Page.html", {
+    var ControlConstructor = WinJS.UI.Pages.define("/pages/hub/TrackerPage.html", {
         // This function is called after the page control contents 
         // have been loaded, controls have been activated, and 
         // the resulting elements have been parented to the DOM. 
         ready: function (element, options) {
             options = options || {};
+
+            Microsoft.Maps.loadModule('Microsoft.Maps.Map', { callback: initMap, culture: "en-us", homeRegion: "US" });
+            function initMap(divName) {
+                var map;
+
+                if (!divName)
+                    divName = "mapDiv";
+
+                var mapOptions =
+                {
+                    credentials: "AsR2QA0NZcWSui-nFCb50_nSCfoJoSenaR-R3Ha5heqM6OkRVZw-IP2Lml7F2E-K",
+                    center: new Microsoft.Maps.Location(lat, long),
+                    mapTypeId: Microsoft.Maps.MapTypeId.road,
+                    zoom:17
+                };
+
+                map = new Microsoft.Maps.Map(document.getElementById("mapDiv"), mapOptions);
+            }
         },
     });
 
@@ -15,6 +33,6 @@
     // data-win-control attribute. 
 
     WinJS.Namespace.define("HubApps_SectionControls", {
-        Section2Control: ControlConstructor
+        TrackerControl: ControlConstructor
     });
 })();
