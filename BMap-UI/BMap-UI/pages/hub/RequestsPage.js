e3d9eb85406cef77f048dc05dca47b4f3e58c427
Asutosh Palai <asupalai@gmail.com>
2015-02-08 09:11:45 +0530

    Removed Notificatiosn and created Requests

---
+++ BMap-UI/BMap-UI/pages/hub/RequestsPage.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    "use strict";
+
+    var ControlConstructor = WinJS.UI.Pages.define("/pages/hub/RequestsPage.html", {
+        // This function is called after the page control contents 
+        // have been loaded, controls have been activated, and 
+        // the resulting elements have been parented to the DOM. 
+        ready: function (element, options) {
+            options = options || {};
+            var frndlist = "";
+            if (WinJS.Application.sessionState.frndlist == "0") {
+                frndlist = "Sorry! Unable to fetch your friendlist.";
+            }
+            else {
+
+
+                var details = WinJS.Application.sessionState.frndlist;
+                console.log(details);
+                frndlist = '<div>';
+                for (var i = 0 ; i < user.people.length ; i++) {
+                    frndlist += '<div class="label-medium">';
+                    frndlist += details['people'][i]['name'];
+                    frndlist += '</div>';
+                    console.log(details);
+                }
+                frndlist += '</div>';
+            }
+            $("#Requests").html(frndlist);
+        }
+    });
+
+    // The following lines expose this control constructor as a global. 
+    // This lets you use the control as a declarative control inside the 
+    // data-win-control attribute. 
+
+    WinJS.Namespace.define("HubApps_SectionControls", {
+        RequestsControl: ControlConstructor
+    });
+})();
--- BMap-UI/BMap-UI/pages/hub/TrackerPage.js
+++ BMap-UI/BMap-UI/pages/hub/TrackerPage.js
@@ -1,6 +1,6 @@
 ﻿(function () {
     "use strict";
-
+    
     var ControlConstructor = WinJS.UI.Pages.define("/pages/hub/TrackerPage.html", {
         // This function is called after the page control contents 
         // have been loaded, controls have been activated, and 
